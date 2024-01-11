091f04c28714df000d381255016639e73df31032
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-01-11 18:44:23 +0530

    token auth implemented

--- controller/userController.js
+++ controller/userController.js
@@ -2,17 +2,17 @@ import expressAsyncHandler from "express-async-handler";
 
 import bcrypt from 'bcrypt'
 import User from "../model/userModel.js";
+import jwt from 'jsonwebtoken';
+const register = expressAsyncHandler(async (req, res) => {
 
-const register = expressAsyncHandler(async(req,res)=>{
 
-    
-    const {username,email,password} = req.body;
+    const { username, email, password } = req.body;
 
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
         res.status(400).send("All fields are required")
     }
 
-    const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const newUser = await User.create({
         username,
@@ -22,22 +22,47 @@ const register = expressAsyncHandler(async(req,res)=>{
         console.error('Error creating user:', error);
     });
 
-   
-    if(newUser){
-        res.status(201).json({message:"User Created Successfully",email:newUser.email})
+
+    if (newUser) {
+        res.status(201).json({ message: "User Created Successfully", email: newUser.email })
 
     }
-    else{
+    else {
         res.status(400);
     }
 
 
 })
 
-const login = expressAsyncHandler(async(req,res)=>{
+const login = expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required." });
+    }
 
-    
-})
+    const user = await User.findOne({ where: { email } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                },
+            },
+            "thetokensecret",
+            { expiresIn: "20m" }
+        );
+
+        return res.status(200).json({ accessToken });
+    } else {
+        return res.status(401).json({ message: "Invalid email or password." });
+    }
+});
 
 
-export {register,login};
+const currentUser = expressAsyncHandler(async (req, res) => {
+    res.json(req.user);
+  });
+
+export { register, login ,currentUser};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controller/userController.js";
-
+import { currentUser, login, register } from "../controller/userController.js";
+import validateToken from "../middlewares/validation.js";
 const userRoute= Router();
 
 userRoute.post('/register',register)
 userRoute.post('/login',login)
 
+userRoute.get('/current',validateToken,currentUser)
 
 export default userRoute;
