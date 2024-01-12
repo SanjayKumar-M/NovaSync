391e5c4533d8ac94cc3f128b3a454ea342312cd9
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-01-12 18:41:16 +0530

    docker update

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,6 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 import { Agenda, Reminder } from "../model/taskModel.js";
 import { currentUser } from "./userController.js";
 import { emailNotification } from "../notifications/emailNotification.js";
+import schedule from 'node-schedule'
 
 const task = expressAsyncHandler(async (req, res) => {
     try {
@@ -14,10 +15,19 @@ const task = expressAsyncHandler(async (req, res) => {
 
             if (newAgenda) {
                 await emailNotification(
-                    req.currentUser.email,  // Corrected: Use currentUser.email to get the user's email
+                    req.currentUser.email, 
                     `Your agenda scheduled successfully`, 
                     `${newAgenda.agenda} scheduled on ${newAgenda.date}`
                 );
+                const scheduledTime = new Date(`${newAgenda.date} ${newAgenda.time}`);
+                schedule.scheduleJob(scheduledTime, async () => {
+                    await emailNotification(
+                        req.currentUser.email,
+                        `It's time to go`,
+                        `Hey ${req.currentUser.username} now it's time to attend ${newAgenda.agenda} ,Here is your description ${newAgenda.description}`
+                    );
+                });
+
                 res.status(201).json({ message: `Your Agenda scheduled successfully on ${newAgenda.date}` });
             } else {
                 res.status(500).json({ message: 'Internal Server Error' });
@@ -28,7 +38,17 @@ const task = expressAsyncHandler(async (req, res) => {
             });
 
             if (newReminder) {
-                await emailNotification(currentUser.email, `Your Reminder scheduled successfully`, `${newReminder.reminder} scheduled on ${newReminder.date}`);
+                await emailNotification(currentUser.email, `Your Reminder set successfully`, `${newReminder.reminder} scheduled on ${newReminder.date}`);
+
+                const scheduledTime = new Date(`${newReminder.date} ${newReminder.time}`);
+                
+                schedule.scheduleJob(scheduledTime, async () => {
+                    await emailNotification(
+                        req.currentUser.email,
+                        `It's time to go`,
+                        `Hey ${req.currentUser.username} now it's time to attend ${newReminder.agenda} ,Here is your description ${newReminder.description}`
+                    );
+                });
                 res.status(201).json({ message: `Your Reminder scheduled successfully on ${newReminder.date} ` });
             } else {
                 res.status(500).json({ message: 'Internal Server Error' });
