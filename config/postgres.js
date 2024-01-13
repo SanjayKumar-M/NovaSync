989050df7a552f1111a2bb7456d0f71c388dac75
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-01-13 16:29:07 +0530

    update

--- config/postgres.js
+++ config/postgres.js
@@ -7,8 +7,8 @@ const PostgresConnection = async() =>{
   const DB_USER_NAME = process.env.DB_USER_NAME;
   const DB_PASSWORD = process.env.DB_PASSWORD;
   const DATABASE = process.env.DATABASE_NAME;
-
-  const sequelize = new Sequelize(`postgres://${DB_USER_NAME}:${DB_PASSWORD}@localhost:5432/${DATABASE}`)
+  const POSTGRE_URL = process.env.DB_URL;
+  const sequelize = new Sequelize(POSTGRE_URL)
     try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
@@ -18,5 +18,6 @@ const PostgresConnection = async() =>{
   }
   return sequelize;
 }
+
 
 export {Sequelize, PostgresConnection}
